b6bee0a5ef10d4dc1b3feaf4b9a720ecd2e150ef
Omar Hossam <contact@omarhossam.com>
2024-05-06 17:37:50 +0300

    dashboard

--- dashboard.js
+++ dashboard.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.getElementById("total_incidents").textContent = (parentsCount * (0.15 + 0.09 + 0.088)).toFixed(0);
     
     document.getElementById("custom_graphics").setAttribute("href", custom_graphics);
-    console.log("TOOOO");
+    console.log("Teeee");
     console.log(school_buildings);
     //usersPerMonthChart
     new Chart(document.getElementById("usersPerMonthChart"), {
